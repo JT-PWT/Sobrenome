50f926ef1fdc5f2389a8bc7b8cf5299cbac41b89
Jeferson <jeferson.silva220@gmail.com>
2024-09-04 21:31:09 -0300

    Add files via upload

---
+++ nomecompleto.js
@@ -0,0 +1,11 @@
+/*Declare duas variáveis chamadas "nome" e "sobrenome",
+atribua o seu nome e sobrenome a elas. Concatene-as em uma terceira
+variável chamada "nomeCompleto" e imprima ao usuário*/
+
+nome = "Jeferson"
+
+sobrenome = " Teodoro"
+
+nomeCompleto = nome + sobrenome
+
+console.log(nomeCompleto);
---
+++ style.js
@@ -0,0 +1,12 @@
+
+var nome = "João Kleber"
+console.log(nome)
+var nome = "Maria Aparecida Teodoro da Silva"
+console.log(nome)
+var casa = "Cafundó"
+console.log(casa)
+var Hobby = "Bike"
+console.log(Hobby)
+var aula = "Senai"
+console.log(aula)
+
